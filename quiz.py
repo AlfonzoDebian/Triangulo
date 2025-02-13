78ed5d7837f493ddaadf187d4d2f26a9222b0df8
AlfonzoDebian <luismarquez@colegioguanenta.edu.co>
2025-02-13 15:12:43 -0500

    update

--- quiz.py
+++ quiz.py
@@ -5,8 +5,25 @@ print("------------------------------")
 print("----- VERIFICAR TRIANGULO ----")
 print("------------------------------")
 
-# input
+
+# Input
+print("===================================")
+a = int(input("Inserte el valor a: "))
+b = int(input("Inserte el valor b: "))
+c = int(input("Inserte el valor c: "))
+print("===================================")
 
 # processing
+if a + b <= c:
+    r = "No se puede formar un triangulo. "
+elif a + c <= b:
+    r = "No se puede formar un triagulo. "
+elif b + c <= a:
+    r = "No se puede formar un dialogo. "
+else:
+    r = "Se puede formar un triangulo. "
+pass
 
 # output
+
+print(r)
